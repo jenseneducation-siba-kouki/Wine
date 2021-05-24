81627c20042b4550ed2f14d0337930283b9b075c
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2021-05-24 15:05:00 +0200

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const Datastore = require('nedb')
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const app = express();
+const User = require('./models/User.js');
+const { verify } = require('./auth')
 
+const Cryptr = require('cryptr')
+const cryptr = new Cryptr(process.env.SECRET)
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/api/register', async (request, response) => {
+	const user = await User.register(request.body);
+
+	if (user) {
+		response.status(201).json(user);
+	} else {
+		response.status(500).json({ error: "Something went wrong" })
+	}
+})
+
+app.post('/api/login', async (request, response) => {
+	const user = await User.auth(request.body);
+
+	if (user) {
+		response.json(user);
+	} else {
+		response.send("please try again")
+	}
+})
 
 
 
