609c31b7567c77e789e3a8663cf11f88fb4a860f
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2021-05-24 15:02:01 +0200

    updated user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,5 +8,50 @@ const dbUsers = new Datastore
 		autoload: true
 	});
 module.exports = {
-    
+async register(body) {
+		
+		const passwordHash = await bcrypt.hash(body.password, 10);
+		const newUser = {
+			name: body.name,
+            last: body.last,
+            adress: body.adress,
+            age: body.age,
+            repeatPassword: body.repeatPassword,
+			password: passwordHash
+		}
+		return await dbUsers.insert(newUser)
+	},
+
+	async auth(body) {
+		const password = body.password;
+		const user = await dbUsers.findOne({ username: body.username });
+
+		
+		if (!user) {
+			return false;
+		} else {
+		
+			const CHECK = await bcrypt.compare(password, user.password);
+			
+			if (CHECK) {
+				const SECRET = process.env.SECRET;
+
+				const payload = {
+					userID: user._id
+				}
+
+			
+				const token = jwt.sign(payload, SECRET)
+
+				return {
+					token,
+					username: user.username
+				}
+			} else {
+				
+				return false;
+			}
+		};
+	}
+
 }
